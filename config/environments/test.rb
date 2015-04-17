1982d06e4a8d8c82c5f532c2504e8a58f73f5cf3
Nuno Gomes <nuno_andre_7@hotmail.com>
2015-04-17 14:39:51 +0100

    Email configuration added

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,18 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  ActionMailer::Base.smtp_settings = {
+                    :address        => "smtp.gmail.com",
+                    :port           => 587,
+                    :authentication => :plain,
+                    :enable_starttls_auto => true,
+                    :user_name      => "ess.project.g1@gmail.com",
+                    :password       => "project_g1"
+}
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
