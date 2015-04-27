83ee939ddaffc36a87f8467b795978310568612a
Nuno Gomes <nuno_andre_7@hotmail.com>
2015-04-27 16:33:16 +0100

    Validations added to user model
    Validations added;
    First and last name fields added.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   before_filter :update_sanitized_params, if: :devise_controller?
 
   def update_sanitized_params
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :first_name, :last_name, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email,  :first_name, :last_name, :password, :password_confirmation, :current_password) }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 20150427155555) do
   add_index "sponsors", ["user_id"], name: "index_sponsors_on_user_id"
 
   create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
