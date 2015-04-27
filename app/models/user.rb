cb74b7d6d5b9f359b6380808c2fff020275f5bb2
Nuno Gomes <nuno_andre_7@hotmail.com>
2015-04-27 16:33:16 +0100

    Validations added to user model
    Validations added;
    First and last name fields added.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, format: EMAIL_REGEX, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,8 @@
 ActiveRecord::Schema.define(version: 20150417121627) do
 
   create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
