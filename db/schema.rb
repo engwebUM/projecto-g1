2361bd95309df726dc61f65e1831a042522b0b71
nunodio <nunossdionisio@gmail.com>
2015-04-27 11:47:13 +0100

    Schema updated with Plans table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150417121627) do
+ActiveRecord::Schema.define(version: 20150427112842) do
+
+  create_table "plans", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "slots"
+    t.float    "price"
+    t.integer  "tickets"
+    t.float    "discount_coupon"
+    t.integer  "discount_tickets"
+    t.text     "description"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
