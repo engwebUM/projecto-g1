947362db908fffeb7e48f3db6474065933614200
Frederico Carvalho <fred.1992carvalho@gmail.com>
2015-05-04 12:20:13 +0100

    Add sponsor done

---
+++ app/controllers/sponsors_controller.rb
@@ -0,0 +1,36 @@
+class SponsorsController < ApplicationController
+  before_filter :authenticate_user!
+
+  FIRST_CONTACT_STATE = 'First Contact'
+  FINISHED_CONTACT_STATE = 'Finished'
+
+  STATES = [[FIRST_CONTACT_STATE, 1], [FINISHED_CONTACT_STATE, 2]]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @sponsor = Sponsor.new
+    @steps = STATES
+  end
+
+  def create
+    @sponsor = Sponsor.new(sponsor_params)
+    if @sponsor.save
+      flash[:success] = 'Sponsor was successfully created.'
+      redirect_to sponsors_path
+    else
+      flash[:danger] = 'Sponsor was unsuccessfully created.'
+      render :new
+    end
+  end
+
+  private
+
+  def sponsor_params
+    params.require(:sponsor).permit(:organization, :person_name, :person_role, :person_email, :person_phone, :extra_pay, :state, :next_step, :user_id, :plan_id, :notes)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   resources :plans
+  resources :sponsors
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
