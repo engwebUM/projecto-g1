e8a16a923fbc8a3c3b07a0c043ea6ca9d3558190
Nuno Gomes <nuno_andre_7@hotmail.com>
2015-06-01 17:50:36 +0100

    Tasks pagination added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-    @user_unfinished_tasks = Sponsor.where(user: current_user).where.not(state: State.closed)
-    @finished_tasks = Sponsor.where(state: State.closed)
+    @user_unfinished_tasks = Sponsor.where(user: current_user).where.not(state: State.closed).paginate(page: params['page_unfinished'])
+    @finished_tasks = Sponsor.where(state: State.closed).paginate(page: params['page_finished'])
   end
 end
