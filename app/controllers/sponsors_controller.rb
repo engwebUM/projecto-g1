85039ea970e2cf6342bcd53cf290b8271440c4d7
Frederico Carvalho <fred.1992carvalho@gmail.com>
2015-06-01 11:59:27 +0100

    Show page done, as some improvement on other parts of the code

--- app/controllers/sponsors_controller.rb
+++ app/controllers/sponsors_controller.rb
@@ -1,14 +1,10 @@
 class SponsorsController < ApplicationController
   before_filter :authenticate_user!
-
-  FIRST_CONTACT_STATE = 'First Contact'
-  FINISHED_CONTACT_STATE = 'Finished'
-
-  STATES = [[FIRST_CONTACT_STATE, 1], [FINISHED_CONTACT_STATE, 2]]
+  before_action :set_sponsor, only: [:show, :edit, :update, :destroy]
+  before_action :set_states, only: [:index, :new,  :create, :edit, :update]
 
   def index
     @sponsors = Sponsor.all
-    @steps = STATES
   end
 
   def show
@@ -16,12 +12,10 @@ class SponsorsController < ApplicationController
 
   def new
     @sponsor = Sponsor.new
-    @steps = STATES
   end
 
   def create
     @sponsor = Sponsor.new(sponsor_params)
-    @steps = STATES
     if @sponsor.save
       flash[:success] = 'Sponsor was successfully created.'
       redirect_to sponsors_path
@@ -31,21 +25,35 @@ class SponsorsController < ApplicationController
     end
   end
 
+  def destroy
+    if @sponsor.destroy
+      flash[:success] = 'Sponsor was successfully removed'
+    else
+      flash[:danger] = 'Sponsor was not removed'
+    end
+    redirect_to sponsors_path
+  end
+
   def edit
-    @sponsor = Sponsor.find(params[:id])
-    @steps = STATES
   end
 
   def update
-    @sponsor = Sponsor.find(params[:id])
     if @sponsor.update(sponsor_params)
-      redirect_to bookings_path
+      redirect_to sponsors_path
     else
       render 'edit'
     end
   end
 
   private
+
+  def set_states
+    @states = State.all
+  end
+
+  def set_sponsor
+    @sponsor = Sponsor.find(params[:id])
+  end
 
   def sponsor_params
     params.require(:sponsor).permit(:organization, :person_name, :person_role, :person_email, :person_phone, :extra_pay, :state, :next_step, :user_id, :plan_id, :notes)
