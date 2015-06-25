3d36278386acca859d8ac6d306d8c29629e17f5b
nunodio <nunossdionisio@gmail.com>
2015-06-25 04:05:34 +0100

    Refactoring Graphic model

--- app/models/graphic.rb
+++ app/models/graphic.rb
@@ -1,29 +1,40 @@
+class ReturnInvestment
+  def data(plans)
+    plans.each_with_object({}) do |plan, graph_data|
+      graph_data[plan.name] = plan.return_of_investment.round(2)
+    end
+  end
+end
+
+class ExtraPay
+  def data(plans)
+    plans.each_with_object({}) do |plan, graph_data|
+      graph_data[plan.name] = plan.extra_pay_total.round(2)
+    end
+  end
+end
+
+class Profit
+  def data(plans)
+    plans.each_with_object({}) do |plan, graph_data|
+      graph_data[plan.name] = plan.profit.round(2)
+    end
+  end
+end
+
 class Graphic
+  GRAPHS = {
+    roi: ReturnInvestment,
+    extra_pay: ExtraPay,
+    profit: Profit
+  }
+  private_constant :GRAPHS
+
   def initialize(plans)
     @plans = plans
   end
 
-  def return_of_investiment
-    @return_of_investiment = {}
-    @plans.each do |plan|
-      @return_of_investiment[plan.name] = (plan.return_of_investiment).round(2)
-    end
-    @return_of_investiment
-  end
-
-  def return_extra_pay_total
-    @return_extra_pay_total = {}
-    @plans.each do |plan|
-      @return_extra_pay_total[plan.name] = (plan.extra_pay_total).round(2)
-    end
-    @return_extra_pay_total
-  end
-
-  def return_profit
-    @return_profit = {}
-    @plans.each do |plan|
-      @return_profit[plan.name] = (plan.profit).round(2)
-    end
-    @return_profit
+  def data_for(graph_identifier = :roi)
+    GRAPHS[graph_identifier].new.data(@plans)
   end
 end
