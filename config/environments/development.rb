2c1655eeef9d3d6d0fbbba8097aeaa5ffa225e47
Nuno Gomes <nuno_andre_7@hotmail.com>
2015-06-27 14:26:18 +0100

    Trying solve problem with authentication of ActionMailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,12 +6,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   ActionMailer::Base.smtp_settings = {
-                    :address        => 'smtp.gmail.com',
+                    :address        => "smtp.gmail.com",
                     :port           => 587,
                     :authentication => :plain,
                     :enable_starttls_auto => true,
-                    :user_name      => 'ess.project.g1@gmail.com',
-                    :password       => 'project_g1'
+                    :user_name      => "ess.project.g1@gmail.com",
+                    :password       => "project_g1"
   }
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
